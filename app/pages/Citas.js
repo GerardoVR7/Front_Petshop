58ac7a8da5e634b21adb8ca73cfcfc3366c33186
christian0412-perez <193256@ids.upchiapas.edu.mx>
2021-04-23 04:39:46 -0500

    recomendaciones

--- app/pages/Citas.js
+++ app/pages/Citas.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import HeaderClient from "../components/HeaderClient";
-import Calendar from "react-calendar";
+
 
 class Citas extends React.Component{
     render(){
